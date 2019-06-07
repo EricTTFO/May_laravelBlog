285874437742ef9580b69d32c06f3dedcafab7d5
EricTTFO <34400777+EricTTFO@users.noreply.github.com>
2019-06-07 13:27:51 +0800

    S

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,33 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('hello');
+// });
+Route::match(['get', 'post', 'put'], '/', function () {
+    $url = route('hello');
+    // return view('welcome')->with('msg', $url);
+    return redirect()->route('hello');
+});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::post('/', function () {
+//     return view('welcome')->with('msg', '199');
+// });
+// Route::get('/hello',['as' => 'hello', function(){
+//     return view('hello');
+// }]);
+// Route::any('/hello/{id}/comments/{comment?}', function ($id, $commentId = 'aaa') {
+//     return view('welcome')->with('msg', '账号:' . $id . '密码:' . $commentId);
+// })->where(['id' => '[0-9]+', 'comment' => '[a-zA-Z]+']);
+Route::group(['prefix' => 'admin', 'domain' => 'www.laraverblog.com'], function () {
+    Route::get('user/{id}', function ($id) {
+        return view('welcome')->with('msg', $id);
+    });
+    Route::group(['prefix' => 'detail'], function () {
+        Route::get('hello/{id}', function ($id) {
+            return view('welcome')->with('msg', $id);
+        });
+    });
 });
